5b2f81c66d281eeebfeac09c25f5f6cb2cedc867
Mansi Sindhu <mansisindhu2909@gmail.com>
2022-01-17 17:07:46 +0530

    Added pagination in public-albums-api

--- src/controllers/albums.controller.js
+++ src/controllers/albums.controller.js
@@ -5,8 +5,16 @@ const Albums = require("../models/album.model");
 
 router.get("/", async (req, res) => {
   try {
-    const albums = await Albums.find({}).lean().exec();
-    return res.status(200).send(albums);
+    const page = Number(req.query.page) || 1;
+    const size = Number(req.query.size) || 3;
+    const offset = (page - 1) * size;
+
+    const albums = await Albums.find().skip(offset).limit(size).lean().exec();
+
+    const totalCount = await Albums.find().countDocuments();
+    const totalPages = Math.ceil(totalCount / size);
+
+    return res.status(200).send({ albums, totalPages });
   } catch (err) {
     return res.status(404).send({ err });
   }
